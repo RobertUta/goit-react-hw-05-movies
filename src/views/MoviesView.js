23c740511faa7d9a2bbd5be77a01e6e286e1fad7
Uță Robert Constantin <roy_robert_99@yahoo.com>
2024-02-13 15:12:43 +0200

    Update MoviesView.js

--- src/views/MoviesView.js
+++ src/views/MoviesView.js
@@ -8,39 +8,45 @@ import qs from 'qs';
 import { QueryContext } from '../Components/App';
 
 const MoviesView = () => {
-  const [movies, setMovies] = useState([]);
+  // component to render search form and movies list
+  const [movies, setMovies] = useState([]); // state to store fetched movies
 
-  const { query, setQuery } = useContext(QueryContext);
-  const { search, pathname } = useLocation();
-  const navigate = useNavigate();
+  const { query, setQuery } = useContext(QueryContext); // get query state and setQuery function from context
+  const { search, pathname } = useLocation(); // hook to get current url
+  const navigate = useNavigate(); // hook to navigate to another route
 
   useEffect(() => {
+    // eslint-disable-line react-hooks/exhaustive-deps
     if (search.includes('query')) {
-      setQuery(qs.parse(search.replace('?', '')).query);
-      return;
+      // check if url contains query
+      setQuery(qs.parse(search.replace('?', '')).query); // update query state with query from url
+      return; // stop execution
     }
-    setQuery('');
+    setQuery(''); // clear query state if url doesn't contain query
   }, [search]); // eslint-disable-line react-hooks/exhaustive-deps
 
   useEffect(() => {
-    let cancelled = false;
+    // eslint-disable-line react-hooks/exhaustive-deps
+    let cancelled = false; // flag to check if component is unmounted
 
-    !query && setMovies([]);
+    !query && setMovies([]); // clear movies list if query is empty
 
     if (query) {
-      navigate(`${pathname}?query=${query}`);
-      getMoviesByQuery(query)
-        .then(data => !cancelled && setMovies(data))
-        .catch(() => Notify.failure('Something went wrong!'));
+      navigate(`${pathname}?query=${query}`); // update url with query
+      getMoviesByQuery(query) // fetch movies by query
+        .then(data => !cancelled && setMovies(data)) // update state with fetched data
+        .catch(() => Notify.failure('Something went wrong!')); // show error notification
     }
     return () => {
-      cancelled = true;
+      // cleanup function
+      cancelled = true; // set flag to true to prevent updating state after component is unmounted
     };
   }, [query]); // eslint-disable-line react-hooks/exhaustive-deps
 
-  const onSubmit = query => setQuery(query);
+  const onSubmit = query => setQuery(query); // update query state with input value
 
   return (
+    // render search form and movies list
     <>
       <SearchForm onSubmit={onSubmit} />
       <MoviesList movies={movies} />
